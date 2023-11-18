3068812a2aa42641d80b0c6bfe12642c57845867
puggeraponanna <ponannapuggera@gmail.com>
2023-11-18 11:45:51 +0530

    Config changes

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,6 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
         { 'rose-pine/neovim',                 name = 'rose-pine' },
+        { 'folke/neodev.nvim' },
         { 'neovim/nvim-lspconfig' },
         { 'williamboman/mason.nvim' },
         { 'williamboman/mason-lspconfig.nvim' },
@@ -25,10 +26,6 @@ require('lazy').setup({
         { 'nvim-lua/plenary.nvim' },
         { 'nvim-lualine/lualine.nvim' },
         { 'hrsh7th/cmp-nvim-lsp' },
-        { 'hrsh7th/cmp-nvim-lua' },
-        { 'hrsh7th/cmp-buffer' },
-        { 'hrsh7th/cmp-path' },
-        { 'hrsh7th/cmp-cmdline' },
         { 'hrsh7th/nvim-cmp' },
         { 'L3MON4D3/LuaSnip' },
         { 'saadparwaiz1/cmp_luasnip' },
@@ -36,17 +33,9 @@ require('lazy').setup({
         { 'windwp/nvim-autopairs',            event = "InsertEnter",         opts = {} },
         { 'christoomey/vim-tmux-navigator' },
         { "nvim-neorg/neorg",                 build = ":Neorg sync-parsers", },
-        {
-            "folke/noice.nvim",
-            event = "VeryLazy",
-            dependencies = {
-                "MunifTanjim/nui.nvim",
-                "rcarriga/nvim-notify",
-            }
-        },
         { "lewis6991/gitsigns.nvim" },
         { "numToStr/Comment.nvim" },
-        { "puggeraponanna/rest.nvim" }
+        { "puggeraponanna/rest.nvim" },
     },
     {
         performance = {
@@ -90,6 +79,8 @@ require('rose-pine').setup({
 })
 vim.cmd.colorscheme('rose-pine')
 
+require('neodev').setup()
+
 -- Completion
 local cmp = require("cmp")
 cmp.setup({
@@ -99,48 +90,30 @@ cmp.setup({
         end,
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<tab>'] = cmp.mapping.select_next_item(),
+        ['<S-tab>'] = cmp.mapping.select_prev_item(),
+        ['<CR>'] = cmp.mapping.confirm { select = true },
     }),
+
     sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-    }, {
-        { name = 'buffer' },
     })
 })
 
 -- LSP
 local on_attach = function(_, bufnr)
-    local nmap = function(keys, func, desc)
-        if desc then
-            desc = 'LSP: ' .. desc
-        end
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = bufnr })
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, { buffer = bufnr })
 
-        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-    end
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { buffer = bufnr })
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = bufnr })
 
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = bufnr })
 
-    nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-    nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-
-    nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-    nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-    nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-    nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-
-    nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
-
-    nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-    nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-    nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-    nmap('<leader>wl', function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, '[W]orkspace [L]ist Folders')
-    nmap('<leader>fm', vim.lsp.buf.format, 'Format buffer')
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = bufnr })
+    vim.keymap.set('n', '<leader>fm', vim.lsp.buf.format, { buffer = bufnr })
 end
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
@@ -148,7 +121,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 require("mason").setup({})
 require("mason-lspconfig").setup({})
 
-local servers = { 'lua_ls', 'gopls', 'hls', 'tsserver', 'pyright' }
+local servers = { 'lua_ls', 'gopls', 'hls', 'pyright' }
 
 for _, lsp in ipairs(servers) do
     require("lspconfig")[lsp].setup {
@@ -157,36 +130,20 @@ for _, lsp in ipairs(servers) do
         settings = {
             Lua = {
                 diagnostics = {
-                    globals = { "vim" }
+                    globals = { "vim" },
+                    disable = { "missing-fields" }
                 }
             }
         },
     }
 end
 
--- Noice
-require("noice").setup({
-    lsp = {
-        progress = {
-            enabled = true,
-            format = "lsp_progress",
-            format_done = "lsp_progress_done",
-            throttle = 1000 / 30, -- frequency to update lsp progress message
-            view = "mini",
-        },
-        override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-        },
-    },
-    presets = {
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-    },
-})
-
 -- Lualine
 require("lualine").setup({
+    options = {
+        section_separators = "",
+        component_separators = ""
+    },
     tabline = {
         lualine_a = { 'buffers' },
     },
@@ -194,7 +151,7 @@ require("lualine").setup({
 
 -- Treesitter
 require("nvim-treesitter.configs").setup {
-    ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "go", "haskell" },
+    ensure_installed = { "lua", "vim", "vimdoc", "query", "go", "haskell", "python" },
     highlight = { enable = true },
 }
 
@@ -225,20 +182,17 @@ require("rest-nvim").setup({
 })
 
 -- Other config
-vim.o.termguicolors = true
+vim.opt.termguicolors = true
 
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
---vim.o.updatetime = 250
---vim.o.timeoutlen = 300
-
-vim.o.mouse = 'a'
+vim.opt.mouse = 'a'
 vim.wo.number = true
 
 vim.opt.whichwrap = "<,>,h,l,[,]"
 
-vim.o.hlsearch = false
+vim.opt.hlsearch = false
 
 vim.opt.guicursor = "i:hor20"
 vim.opt.relativenumber = true
@@ -276,10 +230,9 @@ vim.keymap.set("n", "<S-tab>", ":bp<CR>", { silent = true })
 vim.keymap.set("n", "<leader>rr", "<Plug>RestNvim", { silent = true })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "find files" })
-vim.keymap.set('n', '<leader>fa', "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
-    { desc = "find files" })
-vim.keymap.set('n', '<leader>gt', builtin.git_status, { desc = "git status" })
-vim.keymap.set('n', '<leader>fw', builtin.live_grep, { desc = "live grep" })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = "find buffers" })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = "help tags" })
+vim.keymap.set('n', '<leader>fa', "<cmd> Telescope find_files hidden=true <CR>")
+vim.keymap.set('n', '<leader>ff', builtin.find_files)
+vim.keymap.set('n', '<leader>gt', builtin.git_status)
+vim.keymap.set('n', '<leader>fw', builtin.live_grep)
+vim.keymap.set('n', '<leader>fb', builtin.buffers)
+vim.keymap.set('n', '<leader>fh', builtin.help_tags)
